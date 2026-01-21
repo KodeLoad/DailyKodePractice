c38252ba662eecfe8c10ae52ab5d84ddd36780bb
Aniket Vaishnav <37221337+OBrutus@users.noreply.github.com>
2026-01-21 14:17:49 +0530

    chroe: updated the solution with proper documentation comment

--- GeeksForGeeks/StockSpanProblem/Solution.java
+++ GeeksForGeeks/StockSpanProblem/Solution.java
@@ -5,37 +5,62 @@ import java.util.Stack;
 
 /*
 
-    stack
-        0(100) ;; 1(8) ;;
-        0(100) ;; 2(90) ;;
+    0       1       2       3       :: index
+    100     80      90      120     :: prices
+    1       1       2       4   
+    
+    previous elements < or = to cur price
+
+    BF: O(N**2)
+    
+    ofc:
+        what was the last number which are good
+        or last bad number
         
+        100 80 70 40 20 10 90 95
+        any lookup or any ds directly 100
+        
+        for next element if previous was not voilated then
+        the passing elements would be same for cur as well
 
+        one by one 
+        * -> * -> ...
+        LIFO :: stack
 */
-
 class Solution {
-    public ArrayList<Integer> calculateSpan(int[] arr) {
-        // code here
-        ArrayList<Integer> ansList = new ArrayList<>();
+    public ArrayList<Integer> calculateSpan(int[] price) {
+        ArrayList<Integer> res = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
         
-        int n = arr.length;
-        int i = 0;
-        
-        while(i < n){
-            while(!stack.isEmpty() && arr[stack.peek()] <= arr[i]){
+        int itr = 0;
+        while (itr < price.length) {
+            // hey is it violating
+            // if yes remove (pop)
+            while (
+                !stack.isEmpty() &&
+                price[stack.peek()] <= price[itr]
+            ) {
+                // remove unccesary good items
                 stack.pop();
             }
             
-            if(stack.isEmpty()) {
-                ansList.add(i+1);
+            // valid index
+            // count
+            if (stack.isEmpty()) {
+                // there is no such item
+                // all good things
+                res.add(itr + 1);
             } else {
-                ansList.add(i-stack.peek());
+                // there is some one violating
+                res.add(itr - stack.peek());
             }
             
-            stack.push(i);
-            i++;
+            // advance itr and stack
+            stack.push(itr);
+            itr++;
         }
         
-        return ansList;
+        
+        return res;
     }
 }
